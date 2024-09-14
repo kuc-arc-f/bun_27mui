a7b9b85441da98146b003a84a35824ff0d656d1e
中島浩二 <knaka0209@gmail.com>
2024-09-14 11:56:03 +0900

    up, 0914a4

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import Home from './client/home';
 import About from './client/about';
 import Button from './client/Button';
 import Checkbox from './client/Checkbox';
+import Dialog from './client/Dialog';
 import RadioButtonsGroup from './client/RadioButtonsGroup';
 import Select from './client/Select';
+import TextField from './client/TextField';
 // 
 const App = () => {
   return (
@@ -18,8 +20,10 @@ const App = () => {
       <Route path="/about" element={<About />} />
       <Route path="/button" element={<Button />} />
       <Route path="/checkbox" element={<Checkbox />} />
+      <Route path="/dialog" element={<Dialog />} />
       <Route path="/radio" element={<RadioButtonsGroup />} />
       <Route path="/select" element={<Select />} />
+      <Route path="/textfield" element={<TextField />} />
     </Routes>
   </div>
   );
--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -6,10 +6,12 @@ function Page() {
     <div>
         <a href="/">[ Home ]</a>
         <a href="/about" className="ms-2"> [ about ]</a>
-        <a href="/button" className="ms-2"> [ button ]</a>
+        <a href="/button" className="ms-2"> [ Button ]</a>
         <a href="/checkbox" className="ms-2"> [ Checkbox ]</a>
+        <a href="/dialog" className="ms-2"> [ Dialog ]</a>
         <a href="/radio" className="ms-2"> [ Radio ]</a>
-        <a href="/select" className="ms-2"> [ select ]</a>
+        <a href="/select" className="ms-2"> [ Select ]</a>
+        <a href="/textfield" className="ms-2"> [ TextField ]</a>
         <hr />
     </div>
     );
